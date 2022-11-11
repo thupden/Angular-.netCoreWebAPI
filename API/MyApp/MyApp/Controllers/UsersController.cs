5c0c6d3f06825e5b69087702a89a7d6329995d05
Thupden <thupdenbhutia281199@gmail.com>
2022-11-11 15:13:28 +0530

    completed code skeleton

---
+++ API/MyApp/MyApp/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
+using MyApp.Entities;
+
+namespace MyApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersById(int id)
+        {
+            var users = await _context.Users.FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(users);
+        }
+    }
+}
---
+++ API/MyApp/MyApp/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Entities;
+
+namespace MyApp.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) 
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
