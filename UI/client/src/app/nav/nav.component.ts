20cf47f6d2b045f74b9b86545a98bbc5c011f970
thupden <kkpz919@astrazeneca.net>
2022-11-30 18:25:41 +0900

    routing added

--- UI/client/src/app/nav/nav.component.ts
+++ UI/client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -10,22 +13,24 @@ export class NavComponent implements OnInit {
   model: any = {};
   
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login()
   {
-    this.accountService.login(this.model).subscribe({next: response => {
-      console.log(response);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+      this.router.navigateByUrl("/members")
     },
-    error: error => console.log(error)
+    error: error => this.toastr.error(error.error)
   })
   }
 
   logout()
   {
     this.accountService.logout();
+    this.router.navigateByUrl("/")
   }
 }
--- UI/client/src/app/register/register.component.ts
+++ UI/client/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
+
 
 @Component({
   selector: 'app-register',
@@ -12,7 +14,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -23,7 +25,11 @@ export class RegisterComponent implements OnInit {
       next:() => {
         this.cancel();
       },
-      error:error => console.log(error)
+      error:error => {
+        this.toastr.error(error.error)
+        console.log(error)
+      }
+      
     })
   }
   cancel()
