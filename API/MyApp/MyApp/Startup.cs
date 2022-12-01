60e8028e00980ef7c41ac29dd44151aec19122a3
thupden <kkpz919@astrazeneca.net>
2022-12-01 20:58:15 +0900

    error-handling-added

--- API/MyApp/MyApp/Startup.cs
+++ API/MyApp/MyApp/Startup.cs
@@ -1,24 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using MyApp.Data;
 using MyApp.Extensions;
-using MyApp.Interfaces;
-using MyApp.Services;
+using MyApp.Middleware;
 
 namespace API
 {
@@ -50,6 +31,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
+            app.UseMiddleware<ExceptionMiddleware>();
             app.UseHttpsRedirection();
 
             app.UseRouting();
